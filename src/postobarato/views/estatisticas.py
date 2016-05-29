dad8408a68a94d88f345b4ba9c59fbf5f20d5ad5
Lucas Ayres <lucasayres@startonapp.com>
2016-05-29 15:51:58 -0300

    adicionando select nas estatisticas

--- src/postobarato/views/estatisticas.py
+++ src/postobarato/views/estatisticas.py
@@ -1,7 +1,10 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, current_app
+import json
 
 mod_estatisticas = Blueprint('mod_estatisticas', __name__)
 
 @mod_estatisticas.route('/estatisticas')
 def estatisticas():
-	return render_template('estatisticas.html')
+	with open(current_app.config['BASE_DIR'] + '/database/capitais.json') as arquivo:
+		dados = json.load(arquivo)
+	return render_template('estatisticas.html', capitais=dados['capitais'])
