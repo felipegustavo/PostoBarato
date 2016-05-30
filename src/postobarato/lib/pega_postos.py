43caf92c9df489b025be81e466a5731e72b2eb52
Felipe Gustavo de Souza Gomes <felipegustavo1000@gmail.com>
2016-05-30 17:35:05 -0300

    Mais uma capital adicionada

--- src/postobarato/lib/pega_postos.py
+++ src/postobarato/lib/pega_postos.py
@@ -5,14 +5,14 @@ import re
 
 if __name__ == "__main__":
 
-	fname = sys.argv[1]
-	name = sys.argv[2]
-	centerlat = sys.argv[3]
-	centerlng = sys.argv[4]
-	swlat = sys.argv[5]
-	swlng = sys.argv[6]
-	nelat = sys.argv[7]
-	nelng = sys.argv[8]
+	fname = sys.argv[1] #nome do arquivo.json
+	name = sys.argv[2] #nome da capital
+	centerlat = sys.argv[3] #lat do centro
+	centerlng = sys.argv[4] #lng do centro
+	swlat = sys.argv[5] #lat do sudoeste
+	swlng = sys.argv[6] #lng do sudoeste
+	nelat = sys.argv[7] #lat do nordeste
+	nelng = sys.argv[8] #lng do nordeste
 
 	data_url = 'http://www.precodoscombustiveis.com.br/mapa/atualiza?swlat='+swlat+'&swlng='+swlng+'&nelat='+nelat+'&nelng='+nelng+'&zoom=13'
 	r = requests.get(data_url)
@@ -22,7 +22,7 @@ if __name__ == "__main__":
 	text = re.sub(r'"\d+":', "\n", text)
 	text = text[1:len(text)-1]
 
-	f = open('database/postos/'+fname, 'w')
+	f = open('../database/postos/'+fname, 'w')
 	f.write('{\n')
 	f.write('\t\"cidade\":\"'+name+'\",\n')
 	f.write('\t\"centro\":{\n')
